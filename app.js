dcc5927793e4077fa778dd58be30e7af1548edbf
Shivani-1524 <67316487+Shivani-1524@users.noreply.github.com>
2021-09-27 11:25:33 +0530

    fix: validation

--- app.js
+++ app.js
@@ -3,27 +3,27 @@ const stocksQuantity = document.querySelector("#stock-quantity");
 const currentPrice = document.querySelector("#current-price");
 const submitBtn = document.querySelector("#submit-btn");
 const outputText = document.querySelector("#output");
-const body = document.body;
+const containerborder = document.querySelector(".container");
 
 outputText.style.borderColor = "transparent";
-body.style.borderColor = "#fdffb6"
+containerborder.style.borderColor = "#fdffb6"
 
 function calcProfitAndLoss(initial, quantity, current) {
     if (initial > current) {
         let loss = ((initial - current) * quantity).toFixed(2);
         let lossPercentage = ((loss / initial) * 100).toFixed(2);
-        body.style.borderColor = "#d90429"
+        containerborder.style.borderColor = "#d90429"
         outputText.style.borderColor = "#d90429";
         showResult(`Hey the profit is ${loss} and the loss percent is ${lossPercentage}% 😫`);
     } else if (current > initial) {
         let profit = ((current - initial) * quantity).toFixed(2);
         let profitPercentage = ((profit / initial) * 100).toFixed(2);
-        body.style.borderColor = "#06d6a0"
+        containerborder.style.borderColor = "#06d6a0"
         outputText.style.borderColor = "#06d6a0";
         showResult(`Hey the profit is ${profit} and the profit percent is ${profitPercentage}% 😎`);
     }
     else {
-        body.style.borderColor = "#8ecae6"
+        containerborder.style.borderColor = "#8ecae6"
         outputText.style.borderColor = "#8ecae6";
         showResult("No gain no pain. its balance 😄")
     }
@@ -33,13 +33,16 @@ function submitHandler() {
     let init = Number(initalPrice.value);
     let qty = Number(stocksQuantity.value);
     let curr = Number(currentPrice.value);
-    if (!isNaN(init) && !isNaN(qty) && !isNaN(curr)) {
-        if (init == null || init == "", qty == null || qty == "", curr == null || curr == "") {
-            showResult("Please Fill All The Fields");
-        } else {
+    if ((!isNaN(init) && !isNaN(qty) && !isNaN(curr)) && (init > 0 && qty > 0 && curr > 0)) {
+        if ((init && qty && curr)) {
             calcProfitAndLoss(init, qty, curr);
+
+        } else {
+            outputText.style.borderColor = "transparent";
+            showResult("Please Fill All The Fields");
         }
     } else {
+        outputText.style.borderColor = "transparent";
         showResult("Enter valid inputs");
     }
 }
